bcdc10babbf9cf5fb12004c340dd8ddcfaab9de1
andrewras58 <andrewras58@gmail.com>
2024-10-16 18:37:33 -0700

    alpha 2 (attribution to TMDB)

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -4,7 +4,7 @@ import './css/Footer.scss'
 function Footer() {
   return (
     <footer>
-      <span>Footer Content</span>
+      <span>This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.</span>
     </footer>
   )
 }
